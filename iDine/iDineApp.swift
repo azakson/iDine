027312477198cfbbc84dfc7042ba295aa12fa040
Avery Zakson <avery.zakson@gmail.com>
2021-02-19 10:38:51 -0600

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Avery Zakson on 2/19/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
